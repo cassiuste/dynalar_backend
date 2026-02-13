cc338349faf82b27500bdd0e322669c1c5129fe5
cassius <cassiustedesco1@gmail.com>
2026-02-13 17:07:39 +0100

    First Commit!

---
+++ src/main/java/com/dynalar/dynalar/DynalarApplication.java
@@ -0,0 +1,13 @@
+package com.dynalar.dynalar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynalarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynalarApplication.class, args);
+	}
+
+}
