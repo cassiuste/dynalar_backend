802c3f3d4f4feec8d0fdbeba640350148ec14190
cassius <cassiustedesco1@gmail.com>
2026-02-20 15:32:31 +0100

    Changed the Login in UserController #7

--- src/main/java/com/dynalar/dynalar/respository/UserRepository.java
+++ src/main/java/com/dynalar/dynalar/respository/UserRepository.java
@@ -1,9 +1,12 @@
-package com.dynalar.dynalar.repository;
+package com.dynalar.dynalar.respository;
+
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.dynalar.dynalar.model.user.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {	
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
 }	
