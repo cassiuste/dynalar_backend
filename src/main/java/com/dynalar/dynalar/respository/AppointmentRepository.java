149a268698bc28c9551ad9f4f3d7f6899f34598c
Bielclon <biellaguna12@gmail.com>
2026-02-26 16:56:47 +0100

    Implementation for creating Appointment and index

---
+++ src/main/java/com/dynalar/dynalar/respository/AppointmentRepository.java
@@ -0,0 +1,11 @@
+package com.dynalar.dynalar.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dynalar.dynalar.model.Appointment;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+
+}
