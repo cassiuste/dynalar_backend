7909fc64ebd09ad8bc3e3df0528abc946e3a523e
Cassius Tedesco <157501700+cassiuste@users.noreply.github.com>
2026-05-18 00:03:09 +0200

    Modified boxcontroller #63

--- src/main/java/com/dynalar/dynalar/controller/BoxController.java
+++ src/main/java/com/dynalar/dynalar/controller/BoxController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,15 +29,12 @@ public class BoxController {
 	private AppointmentRepository appointmentRepository;
 	
 	@PostMapping()
-	public ResponseEntity<Box> createBox(@RequestParam Integer number) {
+	public ResponseEntity<Box> createBox(@RequestBody Box box) {
 		try {
-			if (boxRepository.existsById(number)) {
-				return ResponseEntity.badRequest().build();
-			}
-			
-			Box box = new Box();
-			box.setNumber(number);
-			return ResponseEntity.ok(boxRepository.save(box));
+			if (boxRepository.existsById(box.getNumber())) {
+	            return ResponseEntity.badRequest().build();
+	        }
+	        return ResponseEntity.ok(boxRepository.save(box));
 		} catch (Exception e) {
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
 		}
